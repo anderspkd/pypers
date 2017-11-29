7dda929f4348bac9df16e61425c673cd703e4da6
Anders Dalskov <anderspkd@gmail.com>
2017-11-29 22:19:16 +0100

    Trying to keep everything within 80 characters.

--- test/test_papers.py
+++ test/test_papers.py
@@ -18,8 +18,14 @@ class TestAuthorObjects(_with_db):
         author2 = papers.Author(_pft.author1['firstname'],
                                 _pft.author1['lastname'])
 
-        self.assertNotEqual(author1, author2, 'is supposed to be different objects')
-        self.assertEqual(author1._db_obj, author2._db_obj, 'is supposed to be the same')
+        self.assertNotEqual(
+            author1, author2,
+            'is supposed to be different objects'
+        )
+        self.assertEqual(
+            author1._db_obj, author2._db_obj,
+            'is supposed to be the same'
+        )
 
 
 class TestPaperObjects(_with_db):
@@ -41,8 +47,14 @@ class TestPaperObjects(_with_db):
             file_hash=_pft.paper1['hash']
         )
 
-        self.assertNotEqual(paper1, paper2, 'is supposed to be different')
-        self.assertEqual(paper1._db_obj, paper2._db_obj, 'is supposed to be the same')
+        self.assertNotEqual(
+            paper1, paper2,
+            'is supposed to be different'
+        )
+        self.assertEqual(
+            paper1._db_obj, paper2._db_obj,
+            'is supposed to be the same'
+        )
 
     @clean_db
     def test_make_paper_and_get_metadata(self):
@@ -62,8 +74,10 @@ class TestPaperObjects(_with_db):
         # getting set correctly.
         m = db._PaperMetaData.get(db._PaperMetaData.paper == paper._db_obj)
         self.assertEqual(m.bookmark, 10, 'bookmark (in DB) should be 10')
-        self.assertEqual(m.bookmark, paper.bookmark,
-                         'bookmark (in DB) should equal bookmark of paper object')
+        self.assertEqual(
+            m.bookmark, paper.bookmark,
+            'bookmark (in DB) should equal bookmark of paper object'
+        )
 
     # test that we can iterate through the authors of a paper.
     def test_make_paper_with_authors(self):
@@ -85,18 +99,26 @@ class TestPaperObjects(_with_db):
 
     def test_create_author_from_string(self):
         # create an author from a string
-        author1_str = ','.join([_pft.author1['lastname'], _pft.author1['firstname']])
+        author1_str = ','.join(
+            [_pft.author1['lastname'], _pft.author1['firstname']]
+        )
 
         author1_from_str = papers.Author.from_string(author1_str)
-        author1 = papers.Author(_pft.author1['firstname'], _pft.author1['lastname'])
+        author1 = papers.Author(
+            _pft.author1['firstname'], _pft.author1['lastname']
+        )
 
         self.assertEqual(author1_from_str._db_obj, author1._db_obj)
         self.assertNotEqual(author1_from_str, author1)
 
-        author2_str = ' '.join([_pft.author2['firstname'], _pft.author2['lastname']])
+        author2_str = ' '.join(
+            [_pft.author2['firstname'], _pft.author2['lastname']]
+        )
 
         author2_from_str = papers.Author.from_string(author2_str)
-        author2 = papers.Author(_pft.author2['firstname'], _pft.author2['lastname'])
+        author2 = papers.Author(
+            _pft.author2['firstname'], _pft.author2['lastname']
+        )
 
         self.assertEqual(author2_from_str._db_obj, author2._db_obj)
         self.assertNotEqual(author2_from_str, author1)
